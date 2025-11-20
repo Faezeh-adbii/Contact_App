cc7d57ffe665d3f355aeec9e33a8d1ccfb7acd03
Faezeh <faezehadabi9@gmail.com>
2025-11-20 09:46:33 +0330

    Render Contact

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,16 +1,28 @@
-
-function ContactsList({contacts}) {
-  console.log(contacts);
+function ContactsList({ contacts }) {
   return (
     <div>
-        <div>
-          <h3>Contacts List</h3>
-          <ul>
-            {contacts.map(contact => (<li key={contact.id}>{contact.Name}</li>) )}
-          </ul>
-        </div>
+      <div>
+        <h3>Contacts List</h3>
+        {contacts.length ? (<ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                {contact.Name} {contact.LastName}
+              </p>
+              <p>
+                <span>📧</span> {contact.Email}
+              </p>
+              <p>
+                <span>📞</span> {contact.phone}
+              </p>
+              <button>🗑</button>
+            </li>
+          ))}
+        </ul>) : <p>"No Cantact Yet!"</p> }
+   
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactsList
+export default ContactsList;
